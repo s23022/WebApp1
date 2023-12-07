4e5d8e6eaaded7863d81d6c4dc33eece955f6b5a
hiag yuta <s23022@std.it-college.ac.jp>
2023-12-07 08:07:22 +0000

    修正

--- 1207/paiza.js
+++ 1207/paiza.js
@@ -1,8 +1,8 @@
 const print = console.log
 const stream = require('fs')
-  .readFileSync('./data', 'utf8')
+  .readFileSync('/dev/stdin', 'utf8')
   .trim()
-const lines = stream.splint('\n')
+const lines = stream.split('\n')
 const [a, b] = lines
 
 print(a, b)
